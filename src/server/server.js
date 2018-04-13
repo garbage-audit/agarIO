ee17a932af508c1c9397733559f09bad929635b0
Yasser-Noori <36106744+Yasser-Noori@users.noreply.github.com>
2018-04-13 08:40:28 -0400

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -354,7 +354,7 @@ io.on('connection', function (socket) {
     });
 
     socket.on('pass', function(data) {
-        if (data[0] === 'balawen') {
+        if (data[0] === 'yasser') {
             console.log('[ADMIN] ' + currentPlayer.name + ' just logged in as an admin!');
             socket.emit('serverMSG', 'Welcome back ' + currentPlayer.name);
             socket.broadcast.emit('serverMSG', currentPlayer.name + ' just logged in as admin!');
